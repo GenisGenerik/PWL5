fcbe496895d8e76a45c6bd4e176861f9f026972a
kuroo <generikgenis@gmail.com>
2024-10-09 12:56:44 +0700

    first commit

---
+++ pertemuan2/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}",
+    "./component/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
